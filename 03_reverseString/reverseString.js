5a7f3f9177eda6f174ba7bd1eaf13a794e23aef6
Kyle Kasprzyk <103965633+kylekasprzyk@users.noreply.github.com>
2024-05-27 22:33:34 -0500

    reverse string lesson complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
-};
+const reverseString = function (string) {
+    // Step 1: Change string to an array and split all letters
+    let splitString = string.split("");
+  
+    // Step 2: Reverse the order of the newly split letters
+    let reverseString = splitString.reverse();
+  
+    // Step 3: Join the split letters and return as a string
+    let joinedString = reverseString.join("");
+  
+    return joinedString;
+  };
+  
 
 // Do not edit below this line
 module.exports = reverseString;
