90f90f7a90e8acdd694cb38fde35a720cc3aa37a
Kyle Kasprzyk <103965633+kylekasprzyk@users.noreply.github.com>
2024-05-27 23:25:29 -0500

    5th lesson complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (numberOne, numberTwo) {
+  if (
+    typeof numberOne !== "number" ||
+    typeof numberTwo !== "number" ||
+    numberOne < 0 ||
+    numberTwo < 0
+  ) {
+    return "ERROR";
+  }
 
+  let start = Math.min(numberOne, numberTwo);
+  let end = Math.max(numberOne, numberTwo);
+  let sum = 0;
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
